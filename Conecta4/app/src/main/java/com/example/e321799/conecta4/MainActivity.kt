4b84cf7abd5c9e81d803f207bf27f1dfba575040
SergioMartinezC <sergio.martinez.carrasco@gmail.com>
2019-02-25 16:25:05 +0100

    Inicio proyecto

---
+++ Conecta4/app/src/main/java/com/example/e321799/conecta4/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e321799.conecta4
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
