621b404fc041ba491124818bc3f0925a8d655ba4
SergioMartinezC <47328270+SergioMartinezC@users.noreply.github.com>
2019-03-24 20:21:57 +0100

    Cambios en dimens, creados dimens para varios tamaños de pantalla

--- Conecta4/app/src/main/java/com/example/e321799/conecta4/Activities/EndGamePopUp.kt
+++ Conecta4/app/src/main/java/com/example/e321799/conecta4/Activities/EndGamePopUp.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.hardware.display.DisplayManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.util.DisplayMetrics
 import android.view.Gravity
 import android.view.WindowManager
