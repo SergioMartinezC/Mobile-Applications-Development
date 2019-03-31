e34a4eb3bf24320f19943a2fd62e0b74535d7865
AlvaroCaseiro <44638347+AlvaroCaseiro@users.noreply.github.com>
2019-03-31 12:24:01 +0200

    Xmls donete campeon

--- Conecta4/app/src/main/java/com/example/e321799/conecta4/Activities/RoundAdapter.kt
+++ Conecta4/app/src/main/java/com/example/e321799/conecta4/Activities/RoundAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.e321799.conecta4.Activities
 
+import android.support.v7.view.menu.MenuView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import com.example.e321799.conecta4.Model.Round
 import com.example.e321799.conecta4.R
@@ -13,12 +16,15 @@ class RoundHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     var idTextView: TextView
     var boardTextView: ERView
     var dateTextView: TextView
+    private val item : RelativeLayout
     private lateinit var round: Round
 
     init {
         idTextView = itemView.findViewById(R.id.list_item_id) as TextView
         dateTextView = itemView.findViewById(R.id.list_item_date) as TextView
         boardTextView = itemView.findViewById(R.id.list_item_board) as ERView
+        item = itemView.findViewById(R.id.itemView) as RelativeLayout
+
     }
     fun bindRound(round: Round, listener: (Round) -> Unit) {
         this.round = round
@@ -26,10 +32,11 @@ class RoundHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         dateTextView.text = round.date.substring(0,19)
         boardTextView.setBoard(round.board)
         boardTextView.invalidate()
-        itemView.setOnClickListener{ listener(round)}
+        item.setOnClickListener{ listener(round)}
         boardTextView.setOnClickListener {listener(round)}
         idTextView.setOnClickListener { listener(round) }
         dateTextView.setOnClickListener { listener(round) }
+
     }
 }
 
